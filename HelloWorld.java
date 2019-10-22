87d95f7766daeefbcf2db7dd1bcf771d5bef2bf8
Ubuntu <cquinn209@lab4VM.kmopjl3sxbiexek5rcvv0ou1ng.bx.internal.cloudapp.net>
2019-10-22 16:27:28 +0000

    First commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 me : "HelloWorld.java". */
 class HelloWorld 
 { 
-    // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
+    // ur program begins with a call to main(). 
+    / Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
